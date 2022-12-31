047b3131472f424efb546fb9dd6b913953fd82a4
basokant <basokanthan@gmail.com>
2022-12-31 16:39:28 -0500

    add tailwind color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      darkblue: "#102D40",
+      blue: "#22577A",
+      turquise: "#38A3A5",
+      ocean: "#57CC99",
+      green: "#80ED99",
+      tea: "#C7F9CC",
+      lightCyan: "#E8FFFF",
+      white: "#F6FFFF"
+    },
+    fontFamily: {
+      Poppins: 'Poppins, sans-serif',
+    },
     extend: {},
   },
   plugins: [],
