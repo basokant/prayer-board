ce8a8cfefe13b69727b2f159c3308580caf766c4
basokant <basokanthan@gmail.com>
2023-01-06 01:52:57 -0500

    [WIP] add particles background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,19 @@
 // components/Layout/index.js
+import { StarsContainer } from "./StarsContainer";
 
 import { motion } from "framer-motion";
 import { type ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <motion.div
+  <motion.main
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 0.5 }}
     className="flex flex-col align-middle min-h-screen bg-gray-900 font-Poppins text-gray-300"
   >
+    <StarsContainer/>
     {children}
-  </motion.div>
+  </motion.main>
 );
 export default Layout;
---
+++ src/components/StarsContainer.tsx
@@ -0,0 +1,18 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import * as options from "../../public/particles.json" assert { type: "JSON" };
+
+export function StarsContainer() {
+  // this customizes the component tsParticles installation
+  const customInit = useCallback(async (engine: Engine) => {
+    // this adds the bundle to tsParticles
+    await loadFull(engine);
+  }, []);
+
+  console.log(options)
+  
+  // @ts-ignore
+  return <Particles options={options} init={customInit} className="opacity-20" />;
+}
