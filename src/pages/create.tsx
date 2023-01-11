c7d82eb8ecfbfe717e2f8f577e0618dd71d3251a
basokant <basokanthan@gmail.com>
2023-01-11 00:24:06 -0500

    fix create board pattern regex to only be allowed 1 space between words, add toast errors, and refactor success and error effects to react query handlers

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -31,24 +32,25 @@ const CreateBoard: NextPage = () => {
 
       const slug = slugify(boardName);
       if (boardSlugs.includes(slug)) {
-      alert("Board name already taken. Please try another name.");
+        toast.error("Board name already taken. Please try another name.");
         return;
       }
 
-      createBoard.mutate({ name: boardName, password: boardPassword })
-
-      if (createBoard.isError) {
-        alert("Error creating board. Please try again.");
-        return;
-      }
+      createBoard.mutate({ name: boardName, password: boardPassword }, {
+        onSuccess: (data) => {
+          const slug = data.slug;
+          router.push(`/board/${slug}`);
+          toast.success("Board created successfully! 🎉");
+        },
+        onError: (error) => {
+          toast.error("Error creating board. Please try again.");
+          console.log(error);
+        }
+      })
 
     } else if (allBoardSlugs.isError) 
+      toast.error("Error creating board. Please try again.");
       console.log(allBoardSlugs.error);
-  }
-
-  if (createBoard.isSuccess) {
-    const slug = createBoard.data.slug;
-    router.push(`/board/${slug}`);
   }
 
   return (
@@ -73,8 +75,8 @@ const CreateBoard: NextPage = () => {
       <Layout>
         <Navbar />
         <div className="flex flex-col justify-center flex-1 px-7 sm:px-28 xl:px-[27rem]">
-          {createBoard.isLoading && <div className="text-center">Loading...</div>}
           <h1 className="text-right flex-grow-1 p-3 mb-3 text-3xl md:text-4xl font-semibold rounded-lg border-4 border-slate-800">Create a Board.</h1>
+          { createBoard.isLoading && <div className="text-center">Loading...</div> }
           <form 
             className="flex flex-col gap-4 font-medium"
             onSubmit={submitHandler}
@@ -86,8 +88,9 @@ const CreateBoard: NextPage = () => {
               type="text" 
               placeholder="What's the name of your community?" required autoFocus
               value={boardName} minLength={4} maxLength={35}
-              pattern="[0-9a-zA-Z_]{6,35}"
+              pattern="(\w+( \w+)){4, 35}"
               onChange={(e) => setBoardName(e.target.value)}
+              disabled={createBoard.isLoading}
             />
             <label htmlFor="CreateBoardName">Board Password <span className="text-teal-500">*</span> (6-35 alphanumeric)</label>
             <input className="p-3 px-4 rounded-md border-0 bg-gray-700 focus:outline-teal-500"
@@ -96,18 +99,39 @@ const CreateBoard: NextPage = () => {
               type="password" 
               placeholder="A password for the board to be used by the community." required
               value={boardPassword} minLength={6} maxLength={35}
-              pattern="[0-9a-zA-Z_]{4,35}"
+              pattern="(\w+( \w+)){4, 35}"
               onChange={(e) => setBoardPassword(e.target.value)}
+              disabled={createBoard.isLoading}
             />
             <div className="flex justify-end my-4">
               <input 
                 className="text-lg p-2 px-4 bg-cyan-800 text-cyan-50 rounded-full outline-teal-500 hover:bg-cyan-700 transition-color" 
                 type="submit"
                 value="Create Board"
+                disabled={createBoard.isLoading}
               >
               </input>
             </div>
           </form>
+        </div>
+        <div>
+          <Toaster 
+            position="bottom-center"
+            reverseOrder={true}
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: '#374151',
+                color: '#D1D5DA'
+              },
+              success: {
+                iconTheme: {
+                  primary: '#0C9488',
+                  secondary: '#E4E7EB'
+                }
+              }
+            }}
+          />
         </div>
         <Footer />
       </Layout>
